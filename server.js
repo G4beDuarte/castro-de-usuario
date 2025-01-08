1dc3a871de310dd08b214625d7f26d0c40621269
Gabriel Duarte <gabriel.anpedu@gmail.com>
2025-01-07 23:32:08 -0300

    feat: criando rotas para criar e listar usuarios

--- server.js
+++ server.js
@@ -1,4 +1,23 @@
 import express from 'express'
 
 const app = express()
+app.use(express.json())
 
+/*
+    1- Tipo de Rota / Método HTTP
+    2- Endereço
+*/
+
+const users = []
+
+app.post('/usuarios', (req, res) => {
+    users.push(req.body)
+
+    res.status(201).json(req.body)
+})
+
+app.get('/usuarios', (req, res) => {
+    res.status(200).json(users)
+})
+
+app.listen(3000)
